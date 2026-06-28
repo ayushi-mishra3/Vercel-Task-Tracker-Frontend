d2c71c3c5479ec8da68d19d2a1c0012aab5e3b9c
Ayushi Mishra <ayudon710@gmaiil.com>
2026-06-28 13:10:51 +0530

    changed to backend live link

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,18 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/tasks",
+  baseURL: "https://task-tracker-backend-peach.vercel.app/api/tasks",
 });
 
 export const getTasks = () => API.get("/");
-
 export const createTask = (taskData) => API.post("/", taskData);
-
-export const updateTask = (id, taskData) =>
-  API.put(`/${id}`, taskData);
-
-export const deleteTask = (id) =>
-  API.delete(`/${id}`);
-
-export const toggleStatus = (id) =>
-  API.patch(`/${id}/status`);
+export const updateTask = (id, taskData) => API.put(`/${id}`, taskData);
+export const deleteTask = (id) => API.delete(`/${id}`);
+export const toggleStatus = (id) => API.patch(`/${id}/status`);
